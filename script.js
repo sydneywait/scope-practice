81d7c024824e2388d4fe7e1f517fa130a582905a
sydneywait <sydneywait@gmail.com>
2019-01-24 11:09:37 -0500

    finished mod squad fixes

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ const conjunction = function (firstWord, secondWord) {
 //had to catch the variable in order to use it in te console log
 conjoinedWordPrint = conjunction("Master", "Card")
 console.log(conjoinedWordPrint)
+
+// Mod Squad
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+//cannot append to a const.  Change to Let 
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        
+        HTMLRepresentation += `<div>${member}</div>`
+        console.log(HTMLRepresentation)
+
+        //return the variable so the rest of code can see it
+        return HTMLRepresentation;
+    })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
+//catch the HTMLRepresentation variable coming out of the function
+// catchString = member;
