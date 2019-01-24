edddea1fc9f99c01e98bd62a6e6a882cfcd0363c
sydneywait <sydneywait@gmail.com>
2019-01-24 11:21:15 -0500

    finished invalid location practice

--- script.js
+++ script.js
@@ -35,10 +35,37 @@ let HTMLRepresentation = `<h1>The Mod Squad</h1>`
         HTMLRepresentation += `<div>${member}</div>`
         console.log(HTMLRepresentation)
 
-        //return the variable so the rest of code can see it
-        return HTMLRepresentation;
+        
     })
     document.querySelector(".show-info").innerHTML = HTMLRepresentation
 }
-//catch the HTMLRepresentation variable coming out of the function
-// catchString = member;
+
+
+// Simon Says
+// There are two separate scoping issues in this code. Remember, for and if are their own block scopes and if something is declared with let or const then that variable only exists in that scope.
+
+let locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+// initialize a variable outside the for loop to count invalid locations
+let invalidCount = 0;
+
+for (let k = 0; k < locations.length; k++) {
+    const currentLocation = locations[k]
+//looks a the current locator points within the array.  If Y value is greater than 2, it is invalid.
+    if (currentLocation[1] > 2) {
+        const invalidLocation = true
+        console.log("This location", k-1, "is invalid")
+        //increment the counter for invalid locations
+        invalidCount = invalidCount + 1;
+    }
+//No need for this additional if statement.  Perform all actions inside the other if statement.
+    // if (invalidLocation) {
+      
+
+    // }
+}
+
+/*
+    Since k is used as the loop counter, it will be the
+    total number of locations, not the invalid ones. Use the variable set in the global scope
+*/
+console.log(`There were ${invalidCount} invalid locations`)
