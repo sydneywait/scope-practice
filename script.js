5eba292a5d342187bf5c77bafc318cc3029ffda2
sydneywait <sydneywait@gmail.com>
2019-01-24 11:29:53 -0500

    completed the llama challenge

--- script.js
+++ script.js
@@ -69,3 +69,35 @@ for (let k = 0; k < locations.length; k++) {
     total number of locations, not the invalid ones. Use the variable set in the global scope
 */
 console.log(`There were ${invalidCount} invalid locations`)
+
+
+// Advanced Challenge: Lambda Llama
+// Not only can functions take other functions as arguments, but functions can also return functions! This is marked as an advanced challenge because you haven't been introduced to this concept yet.
+
+// The llamaNamer function defines an internal function and your job is to rewrite this code so that the internal function gets returned.
+
+// Remember, come to the staff and ask questions if you decide to take this one on and get stuck.
+
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        console.log("this is the random name", name);
+        //return the name and suffix
+        return name + suffix
+    }
+//call the function "namer" and catch the name and suffix
+randomNameString = namer()
+console.log("this is the random name string", randomNameString)
+//return the randomNameString from the function so it is available outside namer(), inside llamaNamer()
+return randomNameString;
+    
+}
+//catch the variable randomNameString so you can use it in the global scope (outside llamaNamer)
+nameMaker = llamaNamer()
+
+//nameMaker is a variable, not a function.  Show accordingly.
+console.log(nameMaker)
